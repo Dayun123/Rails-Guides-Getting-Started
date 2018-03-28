abb47f3c8a8b2fe9b579664e99b7306f8ad76e08
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2018-03-28 18:22:35 -0500

    Add destroy link to _comments partial and destroy action to CommentsController.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -12,6 +12,23 @@ class CommentsController < ApplicationController
     redirect_to article_path(@article)
   end
 
+  # destroy the comment related to the article from the URL path /articles/:article_id/comments/:id
+  def destroy
+    # find the associated article that goes with this comment (/articles/:article_id/comments/:id) The only reason we need to query the db here is to get the @article model that the comment will be deleted from so we can call redirect_to on it. We can use the @article.comments.find method or just Comment.find, as both will essentially make the same db call.
+    @article = Article.find(params[:article_id])
+
+    # find the comment associated with this article by the comments id. We can use @article.comments.find or Comment.find, both will take the same param and both will make the same SQL call.
+    # @comment = @article.comments.find(params[:id])
+    @comment = Comment.find(params[:id])
+
+    # destroy the comment
+    @comment.destroy
+
+    # redirect the user to the show view for the article the comment was deleted from. Here we just redirect_to the @article instance variable, and Rails knows to go to the show view for the ArticlesController. The style using the named path helper is commented out below.
+    redirect_to @article
+    # redirect_to article_path(@article)
+  end
+
   private
 
     # sanitize the parameters that we expect when a form is submitted to create a new comment. We require a comment model which has :commenter and :body attributes.
